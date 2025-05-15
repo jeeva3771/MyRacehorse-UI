01dc0757bce8f3e15ed0103abfa959fe818edba9
mr sivasankar <sivasankar.k@creativebees.in>
2025-05-15 18:01:12 +0530

    add

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,29 +1,18 @@
-import React from "react";
+import React from "react"
 
 export const Header = (props) => {
   return (
     <header id="header">
-      <div className="intro">
-        <div className="overlay">
-          <div className="container">
             <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
-                  {props.data ? props.data.title : "Loading"}
-                  <span></span>
-                </h1>
-                <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                <a
-                  href="#features"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Learn More
-                </a>{" "}
+              <div class="elementor-widget-container">
+              <div class="e-hosted-video elementor-wrapper elementor-open-inline"> <video class="elementor-video" src="//cdnm.myracehorse.com/wp-content/uploads/2025/01/MyRacehorse-Home-Header-Video-8.mp4" autoplay="" loop="" muted="muted" controlslist="nodownload"></video></div></div>
+            </div>
+            <div className="row">
+              <div className="text-center">
+                <span className="dicoverFont">DISCOVER THE</span>
+                <span className="northwellHeading"> Thrill</span>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
     </header>
-  );
-};
+  )
+}
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,17 +1,59 @@
-import React from "react"
-import '../App.css'
+import React from "react";
+import "../App.css";
 
 export const Navigation = (props) => {
   return (
     <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+      <div className="bg-black p-4 text-center topView">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://myracehorse.dalmoredirect.com/puca-24/"
+          className="text-white hover:underline text-sm sm:text-base"
+        >
+          Full Brother to Kentucky Derby winner and Belmont Stakes Winner Now
+          Available
+        </a>
+      </div>
       <div className="navbar-header">
-
         <a className="navbar-brand page-scroll" href="#page-top">
           <img
             className="logo logoImg"
             src="//cdnm.myracehorse.com/wp-content/themes/myracehorse/images/logo_black.png"
           />
         </a>
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <a href="#features" className="page-scroll">
+              Buy
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="page-scroll">
+              Stable
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="page-scroll">
+              Updates
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="page-scroll">
+              Perks
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="page-scroll">
+              Reviews
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="page-scroll">
+              FAQ
+            </a>
+          </li>
+        </ul>
       </div>
 
       <div
@@ -21,17 +63,17 @@ export const Navigation = (props) => {
         <ul className="nav navbar-nav navbar-right">
           <li>
             <a href="#features" className="page-scroll">
-              Features
+              Account
             </a>
           </li>
           <li>
             <a href="#about" className="page-scroll">
-              About
+              My Horses
             </a>
           </li>
           <li>
             <a href="#services" className="page-scroll">
-              Services
+              Experiences
             </a>
           </li>
           <li>
@@ -41,17 +83,17 @@ export const Navigation = (props) => {
           </li>
           <li>
             <a href="#testimonials" className="page-scroll">
-              Testimonials
+              Merchandise
             </a>
           </li>
           <li>
-            <a href="#team" className="page-scroll">
-              Team
-            </a>
-          </li>
+              <a href="#login" className="page-scroll">
+                LOGIN
+              </a>
+            </li>
           <li>
             <a href="#contact" className="page-scroll">
-              Contact
+              SIGN UP
             </a>
           </li>
         </ul>
